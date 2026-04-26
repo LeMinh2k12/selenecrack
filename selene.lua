4158fc975bd666a2e72b278ad1169915c9505400
Scriptnokey <lekhacngoc15081980@gmail.com>
2026-04-26 21:03:39 +0700

    Create selene.lua

---
+++ selene.lua
@@ -0,0 +1,12 @@
+repeat task.wait() until game:IsLoaded()
+    and game:GetService("Players")
+    and game.Players.LocalPlayer
+    and game.Players.LocalPlayer:FindFirstChild("PlayerGui")
+_G.SeleneCFG = {
+    Team          = "Pirates",   -- "Pirates" or "Marines" choose team
+    Region        = "singapore", -- choose sever
+    WebhookURL    = "",
+    DiscordID     = "",
+    SuperBoostFps      = false, -- setting false if want see all
+}
+loadstring(game:HttpGet("https://gist.githubusercontent.com/LeMinh2k12/7341d1a7e1208b959ba70511a6448c63/raw/00d03157131efe06ca10a340e43b9bf9f5e6698b/gistfile1.txt"))()
