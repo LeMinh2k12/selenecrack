f96132037992bbb2740ea18d3d22ae18dfd0fa86
Scriptnokey <lekhacngoc15081980@gmail.com>
2026-04-29 18:47:09 +0700

    Update Mainscript.lua

--- Mainscript.lua
+++ Mainscript.lua
@@ -1,4 +1,14 @@
-task.spawn(function()   loadstring(game:HttpGet("https://gist.githubusercontent.com/LeMinh2k12/28a7eb946b9e85d4ef8a34500b3ba029/raw/f0be1a21157e221560af5bca7f597c766515bce4/gistfile1.txt"))()
-end)
-task.spawn(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/LeMinh2k12/selenecrack/refs/heads/main/bounty.lua"))()
-end)
+repeat task.wait() until game:IsLoaded()
+    and game:GetService("Players")
+    and game.Players.LocalPlayer
+    and game.Players.LocalPlayer:FindFirstChild("PlayerGui")
+_G.SeleneCFG = {
+    Team          = "Marines",   -- "Pirates" or "Marines" choose team
+    Region        = "singapore", -- choose sever
+    WebhookURL    = "",
+    DiscordID     = "",
+    SuperBoostFps      = true, -- setting false if want see all
+}
+loadstring(game:HttpGet("https://gist.githubusercontent.com/LeMinh2k12/7341d1a7e1208b959ba70511a6448c63/raw/00d03157131efe06ca10a340e43b9bf9f5e6698b/gistfile1.txt"))()
+
+task.spawn(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/LeMinh2k12/selenecrack/refs/heads/main/bounty.lua"))() end)
